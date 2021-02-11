e9f6bf292d2c115ba1d2eaa3be3908bb20e17d1c
Florian Kainz <kainz@adobe.com>
2021-02-10 18:20:33 -0800

    initializing repository

---
+++ my_learning_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_learning_appApp.swift
+//  my_learning_app
+//
+//  Created by Florian Kainz on 2/10/21.
+//
+
+import SwiftUI
+
+@main
+struct my_learning_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
